a512ffdd2d0a3b56aef2860a3b7ecaa81ba97af5
Blue-Beaker <yyy5566778899@126.com>
2025-05-23 18:36:05 +0800

    add system info command

--- blueberry-bot/plugins/commands/__init__.py
+++ blueberry-bot/plugins/commands/__init__.py
@@ -1,19 +1,33 @@
+from nonebot import on_command,logger
+from nonebot.rule import is_type
+from nonebot.internal.adapter.bot import Bot
 
 from nonebot.adapters.discord.commands import (
     CommandOption,
     on_slash_command,
 )
-import platform
+import platform,psutil
 
 def get_system_info()->str:
     sysinfo=[]
+    uname=platform.uname()
+    sysinfo.append(f"System Info")
+    sysinfo.append(f"{uname.system} {uname.release} {uname.machine}, Python {platform.python_version()}")
+    memory=psutil.virtual_memory()
+    
+    sysinfo.append(f"CPU: {psutil.cpu_count(False)}c{psutil.cpu_count()}t {psutil.cpu_freq().current:.0f}Mhz {psutil.cpu_percent():.1f}%, RAM: {(memory.total-memory.available)/(2<<29):.1f}/{memory.total/(2<<29):.1f}GiB")
+    
     print(platform)
     return "\n".join(sysinfo)
 
-matcher = on_slash_command(
+slash = on_slash_command(
     name="sysinfo",
-    description="显示系统信息")
-@matcher.handle()
+    description="显示运行此Bot的系统信息")
+@slash.handle()
 async def _():
-    await matcher.send("Test Feedback!")
+    await slash.send(get_system_info())
 
+cmd = on_command("sysinfo")
+@cmd.handle()
+async def _():
+    await cmd.send(get_system_info())
