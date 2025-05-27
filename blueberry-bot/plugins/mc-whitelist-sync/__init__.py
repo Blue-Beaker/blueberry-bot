d11eb25e276de25090ad205ccae44d92e5284dc3
Blue-Beaker <yyy5566778899@126.com>
2025-05-27 20:50:47 +0800

    增加功能: 多mc服务器同步白名单

---
+++ blueberry-bot/plugins/mc-whitelist-sync/__init__.py
@@ -0,0 +1,36 @@
+import json
+from nonebot import on_command,logger,on_startswith
+from nonebot.rule import is_type
+from nonebot.adapters.minecraft.bot import Bot
+from nonebot.adapters.minecraft import BasePlayerCommandEvent,MessageEvent
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
+
+CMD_PREFIX_PATH="blueberry-bot/plugins/mc-whitelist-sync/server-prefixes.json"
+
+server_prefixes:dict[str,str]={}
+ops:list[str]=[]
+
+with open(CMD_PREFIX_PATH,"r") as f:
+    configFile:dict=json.load(f)
+    ops=configFile.get("ops",[])
+    server_prefixes=configFile.get("prefixes",{})
+
+def convert_server_command(cmd:str,bot1:Bot,bot2:Bot):
+    prefix1=server_prefixes.get(bot1.self_id,"")
+    prefix2=server_prefixes.get(bot2.self_id,"")
+    return cmd.replace(prefix1,prefix2)
+
+handler_msg = on_startswith("whitelist",is_type(BasePlayerCommandEvent))
+@handler_msg.handle()
+async def _(bot:Bot,event:BasePlayerCommandEvent):
+    cmd=event.message.extract_plain_text()
+    print(cmd,event.player.uuid.__str__())
+    if(not event.player.is_op and not str(event.player.uuid) in ops):
+        return
+    for name,bot2 in bot.adapter.bots.items():
+        if(bot==bot2 or not isinstance(bot2,Bot)):
+            continue
+        msg,result = await bot2.send_rcon_cmd(command=convert_server_command(cmd,bot,bot2))
+        if(result>=1):
+            await bot.send_private_msg(uuid=event.player.uuid,nickname=event.player.nickname,message=f"[§bBlueberry_Bot§r] 来自{name}服务器: \n{msg}")
