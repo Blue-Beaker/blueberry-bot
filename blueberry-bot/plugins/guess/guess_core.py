2139a9c12827f88f901ea0e4b0a0bebcc4bd0ee7
Blue-Beaker <yyy5566778899@126.com>
2025-06-16 17:33:04 +0800

    update guess to implement config to starting guesses and guesses per info

--- blueberry-bot/plugins/guess/guess_core.py
+++ blueberry-bot/plugins/guess/guess_core.py
@@ -28,14 +28,17 @@ class GuessSession:
     mapData:MapData
     map_jsondata:dict
     guesses:int
-    guesses_per_info:int=3
+    guesses_per_info:int
     revealed_info:dict[EntityCategory,int]
     entities:dict[str,int]
     
-    def __init__(self,mapData:MapData,map_exported_data:dict) -> None:
+    def __init__(self,mapData:MapData,map_exported_data:dict,guesses_per_info:int=3,starting_info:int=1) -> None:
         self.mapData=mapData
         
         self.guesses=0
+        self.guesses_per_info=guesses_per_info
+        self.starting_info=starting_info
+        
         self.revealed_info={}
         self.finished=False
         
@@ -103,7 +106,7 @@ class GuessSession:
     
     @property
     def guesses_for_next_info(self):
-        return self.revealed_info.__len__()*self.guesses_per_info
+        return (self.revealed_info.__len__()-self.starting_info+1)*self.guesses_per_info
     
     def on_guess_wrong(self)->str:
         if(self.unrevealed_entities().__len__()>0):
@@ -137,7 +140,16 @@ class GuessSession:
         return "\n".join(feedback)
 
 class GuessManager:
+    
     session:GuessSession|None=None
+    
+    def setGuessesPerInfo(self,count:int):
+        self.guesses_per_info=count
+    def setStartingInfo(self,count:int):
+        self.starting_info=count
+        
+    guesses_per_info=3
+    starting_info=1
     
     def get_session(self) -> GuessSession|None:
         return self.session
@@ -154,14 +166,16 @@ class GuessManager:
         map=MAP_MANAGER.pickMap()
         
             
-        self.session=GuessSession(map,getMapJsonFromPath(map.filePath))
+        self.session=GuessSession(map,getMapJsonFromPath(map.filePath),self.guesses_per_info,self.starting_info)
         return self.session
     
     def start(self)->str:
         if(self.session):
             return "当前有正在进行的guess 请先猜出来或放弃\n"+self.session.get_final_message()
         self.session=self._start_guess()
-        self.session.reveal_info()
+        
+        while len(self.session.revealed_info)<self.starting_info and len(self.session.unrevealed_entities())>0:
+            self.session.reveal_info()
         return "你能根据以下信息猜出这是哪张图吗? 输入&guess 你的答案 以回答\n"+self.session.get_final_message()
     
     def cancel(self)->str:
@@ -190,6 +204,7 @@ class GuessManager:
             dumpdata["revealed_info"]=revealed
             dumpdata["guesses"]=self.session.guesses
             dumpdata["guesses_per_info"]=self.session.guesses_per_info
+            dumpdata["starting_info"]=self.session.starting_info
         else:
             dumpdata["current_map"]=None
             
@@ -208,7 +223,7 @@ class GuessManager:
             session=GuessSession(mapData,jsondata)
             inst.session=session
             
-            info=dumpdata.get("revealed_info",None)
+            info=dumpdata.get("revealed_info",{})
             if isinstance(info,dict):
                 for id,value in info.items():
                     entity=ENTITY_MANAGER.category_data.get(id,None)
@@ -216,12 +231,16 @@ class GuessManager:
                         continue
                     session.revealed_info[entity]=value
                     
-            guesses=dumpdata.get("guesses",None)
+            guesses=dumpdata.get("guesses",0)
             if(isinstance(guesses,int)):
                 session.guesses=guesses
                 
-            guesses_per_info=dumpdata.get("guesses_per_info",None)
+            guesses_per_info=dumpdata.get("guesses_per_info",3)
             if(isinstance(guesses_per_info,int)):
                 session.guesses_per_info=guesses_per_info
+                
+            starting_info=dumpdata.get("starting_info",1)
+            if(isinstance(starting_info,int)):
+                session.starting_info=starting_info
                 
         return inst
--- blueberry-bot/plugins/guess/handler_base.py
+++ blueberry-bot/plugins/guess/handler_base.py
@@ -1,5 +1,24 @@
 
+import json
+
+from nonebot import logger
 from .guess_core import GuessManager
+
+
+CONFIG_PATH="config.json"
+
+server_prefixes:dict[str,str]={}
+ops:list[str]=[]
+guesses_per_info:int=1
+starting_info:int=1
+
+with open(CONFIG_PATH,"r") as f:
+    configFile:dict=json.load(f)
+    guessSection=configFile.get("guess",{})
+    guesses_per_info=guessSection.get("guesses_per_info")
+    starting_info=guessSection.get("starting_info")
+
+logger.info(f"loaded config: {guessSection}")
 
 async def run_command(cmd:str,guessManager:GuessManager)->str|None:
     if cmd.startswith("guess"):
@@ -23,6 +42,8 @@ class GuessManagerInstances:
     def getOrCreateGuessManager(self,key:str):
         manager = self.guessManagers.get(key,None)
         if manager:
+            manager.guesses_per_info=guesses_per_info
+            manager.starting_info=starting_info
             return manager
         self.guessManagers[key]=GuessManager()
         return self.guessManagers[key]
