e3df882875d5eb99a98f477a24a52729a5146d0a
Blue-Beaker <yyy5566778899@126.com>
2026-05-09 18:53:23 +0800

    gdguess 适配 OneBot

--- blueberry-bot/plugins/gdguess/__init__.py
+++ blueberry-bot/plugins/gdguess/__init__.py
@@ -7,7 +7,9 @@ import cv2
 from nonebot import on_command,logger,on_startswith,get_plugin_config,on_type,get_adapter
 from nonebot.rule import is_type
 from nonebot.adapters import Message,Event,Bot
-from nonebot.adapters.discord import Message as DCMessage,Bot as DCBot,MessageSegment,GuildMessageCreateEvent
+from nonebot.adapters.discord import Message as DCMessage,Bot as DCBot,MessageSegment as DCMessageSegment,GuildMessageCreateEvent
+from nonebot.adapters.onebot.v11 import Bot as OBBot, GroupMessageEvent as OBGroupMessageEvent,MessageSegment as OBMessageSegment
+
 from nonebot.params import CommandArg
 import nonebot.config
 from nonebot import get_driver
@@ -43,35 +45,38 @@ async def save_sessions():
 
 gdguess_start = on_command("gdguess_start")
 @gdguess_start.handle()
-async def _(event:Event,args: Message = CommandArg()):
-    await guess_start(gdguess_start,event,args,test=False)
+async def _(bot:Bot,event:Event,args: Message = CommandArg()):
+    await guess_start(bot,gdguess_start,event,args,test=False)
     await gdguess_start.finish()
     
 gdguess_hard = on_command("gdguess_hard")
 @gdguess_hard.handle()
-async def _(event:Event,args: Message = CommandArg()):
-    await guess_start(gdguess_hard,event,args,crop_size=(128,128),test=False)
+async def _(bot:Bot,event:Event,args: Message = CommandArg()):
+    await guess_start(bot,gdguess_hard,event,args,crop_size=(128,128),test=False)
     await gdguess_hard.finish()
     
 gdguess_insane = on_command("gdguess_insane")
 @gdguess_insane.handle()
-async def _(event:Event,args: Message = CommandArg()):
-    await guess_start(gdguess_insane,event,args,crop_size=(64,64),test=False)
+async def _(bot:Bot,event:Event,args: Message = CommandArg()):
+    await guess_start(bot,gdguess_insane,event,args,crop_size=(64,64),test=False)
     await gdguess_insane.finish()
     
 gdguess_extreme = on_command("gdguess_extreme")
 @gdguess_extreme.handle()
-async def _(event:Event,args: Message = CommandArg()):
-    await guess_start(gdguess_extreme,event,args,crop_size=(32,32),test=False)
+async def _(bot:Bot,event:Event,args: Message = CommandArg()):
+    await guess_start(bot,gdguess_extreme,event,args,crop_size=(32,32),test=False)
     await gdguess_extreme.finish()
     
 gdguess_test = on_command("gdguess_test",permission=SUPERUSER)
 @gdguess_test.handle()
-async def _(event:Event,args: Message = CommandArg()):
-    await guess_start(gdguess_test,event,args,test=True)
+async def _(bot:Bot,event:Event,args: Message = CommandArg()):
+    await guess_start(bot,gdguess_test,event,args,test=True)
     await gdguess_test.finish()
 
-async def guess_start(matcher:type[Matcher],event:Event,args: Message = CommandArg(),crop_size:tuple[int,int]=(256,256),test:bool=False):
+async def guess_start(bot:Bot,matcher:type[Matcher],event:Event,args: Message = CommandArg(),crop_size:tuple[int,int]=(256,256),test:bool=False):
+    # Only allow in Certain Adapters
+    if not isSupportedAdapter(bot):
+        return
     args_text=[i.strip() for i in args.extract_plain_text().split(",")]
     
     id=getid(event)
@@ -80,14 +85,14 @@ async def guess_start(matcher:type[Matcher],event:Event,args: Message = CommandA
         await matcher.send("你已经有一个正在进行的游戏了! 继续猜图吧!")
         
         
-    msg=[]
+    lines=[]
     levels:list[int]=[]
     
     empty_search=not args_text or args_text==[""]
     
     if empty_search and session and session.completed:
         levels.extend(session.level_pool)
-        msg.append("继续使用上次的关卡池进行游戏!")
+        lines.append("继续使用上次的关卡池进行游戏!")
     elif empty_search:
         await matcher.send("请输入至少一个List ID! 多个ID请用,分隔")
         return
@@ -96,10 +101,10 @@ async def guess_start(matcher:type[Matcher],event:Event,args: Message = CommandA
         for i in args_text:
             lists=gd_api.getList(i)
             if lists.__len__()>1:
-                msg.append(f"关键词 {i} 查找到多个List. 请输入List ID选择具体的List.")
+                lines.append(f"关键词 {i} 查找到多个List. 请输入List ID选择具体的List.")
                 for l in lists:
-                    msg.append(f"{l.id} = {l.name} by {l.creator}")
-                await matcher.send("\n".join(msg))
+                    lines.append(f"{l.id} = {l.name} by {l.creator}")
+                await matcher.send("\n".join(lines))
                 return
             
             levels.extend(lists[0].levels)
@@ -115,8 +120,10 @@ async def guess_start(matcher:type[Matcher],event:Event,args: Message = CommandA
         f.write(img)
     
     if test:
-        msg.append(f"[TEST] {level.name} by {level.creator}, {level.id}")
-        await matcher.send(DCMessage().append("\n".join(msg)).append(MessageSegment.attachment("guess.png",content=img)))
+        lines.append(f"[TEST] {level.name} by {level.creator}, {level.id}")
+        msg="\n".join(lines)
+        await sendMessageAndImage(bot,matcher,msg,img)
+        
     else:
         crop_width, crop_height = crop_size
         cropped_path = IMAGES_PATH/f"{id}.png"
@@ -133,14 +140,19 @@ async def guess_start(matcher:type[Matcher],event:Event,args: Message = CommandA
         
         session_manager.sessions[id]=GuessSession.start(id,level,crop=(left, top, right, bottom),level_pool=levels)
         
-        msg.append("以下截图是来自哪个关卡呢? 输入 -gdguess 你的答案 以回答")
-        await matcher.send(DCMessage().append("\n".join(msg)).append(MessageSegment.attachment("guess.png",content=loadFile(cropped_path))))
+        lines.append("以下截图是来自哪个关卡呢? 输入 -gdguess 你的答案 以回答")
+        msg="\n".join(lines)
+        await sendMessageAndImage(bot,matcher,msg,loadFile(cropped_path))
     
     
 
 gdguess = on_command("gdguess")
 @gdguess.handle()
-async def _(event:Event,args: Message = CommandArg()):
+async def _(bot:Bot,event:Event,args: Message = CommandArg()):
+    # Only allow in Certain Adapters
+    if not isSupportedAdapter(bot):
+        return
+    
     id=getid(event)
     session=session_manager.sessions.get(id)
     if not session or session.completed:
@@ -150,9 +162,15 @@ async def _(event:Event,args: Message = CommandArg()):
     if session.guess(guess):
         session.completed=True
         msg=f"恭喜你猜对了! 关卡是 {session.level_name} by {session.level_creator}, 你总共猜了 {session.guesses} 次!"
-        await gdguess.send(DCMessage().append(msg).append(MessageSegment.attachment("answer.png",content=guess_utils.draw_rectangle_on_image(DATA_PATH/"images"/f"{id}.webp",session.crop))))
+        
+        await sendMessageAndImage(bot,gdguess,msg,guess_utils.draw_rectangle_on_image(DATA_PATH/"images"/f"{id}.webp",session.crop))
+        # await gdguess.send(DCMessage().append(msg).append(DCMessageSegment.attachment("answer.png",content=guess_utils.draw_rectangle_on_image(DATA_PATH/"images"/f"{id}.webp",session.crop))))
         removeImages(id)
     else:
+        if isinstance(bot,OBBot):
+            if random.randint(0,5)==0:
+                await gdguess.send(f"猜错了! 这是 {session.guesses} 次猜测了, 继续加油!")
+            return
         await gdguess.send(f"猜错了! 这是 {session.guesses} 次猜测了, 继续加油!")
     await gdguess.finish()
     
@@ -168,7 +186,7 @@ async def _(event:Event):
     
     msg=f"游戏结束! 关卡是 {session.level_name} by {session.level_creator}, 你总共猜了 {session.guesses} 次!"
     
-    await gdguess.send(DCMessage().append(msg).append(MessageSegment.attachment("answer.png",content=guess_utils.draw_rectangle_on_image(DATA_PATH/"images"/f"{id}.webp",session.crop))))
+    await gdguess.send(DCMessage().append(msg).append(DCMessageSegment.attachment("answer.png",content=guess_utils.draw_rectangle_on_image(DATA_PATH/"images"/f"{id}.webp",session.crop))))
     removeImages(id)
     await gdguess_giveup.finish()
 
@@ -183,7 +201,14 @@ def roll_until_level(levels:list[int]):
             
     return levelID,img
 
+def isSupportedAdapter(bot:Bot):
+    return isinstance(bot,DCBot) or isinstance(bot,OBBot)
 
+async def sendMessageAndImage(bot:Bot,matcher:type[Matcher],message:str,image:bytes,image_name:str="guess.png"):
+    if isinstance(bot,OBBot):
+        await matcher.send(OBMessageSegment.text(message)+OBMessageSegment.image(image))
+    else:
+        await matcher.send(DCMessage().append(message).append(DCMessageSegment.attachment(image_name,content=image)))
 
 def getid(event: Event) -> str:
     if isinstance(event,GuildMessageCreateEvent):
