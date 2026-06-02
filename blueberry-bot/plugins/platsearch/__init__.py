5915adb954a2bc990152568ff0defe79f8dda13a
Blue-Beaker <yyy5566778899@126.com>
2026-06-02 15:39:44 +0800

    removed deprecated functions

--- blueberry-bot/plugins/platsearch/__init__.py
+++ blueberry-bot/plugins/platsearch/__init__.py
@@ -253,13 +253,7 @@ async def _(args: Message = CommandArg()):
     
     if not fuzzy:
         msg.append(f"Exact match, use -f [levelname] for fuzzy search")
-    # results:list[plat_sheets.PlatChartEntry]=[]
-    # levels=plat_sheets.get_plat_chart()
     
-    # for l in levels:
-    #     if search in l.name.lower():
-    #         results.append(l)
-    # results=search_in_levels(PLAT_CHART_CACHE.get(),search,fuzzy)
     results=[l for l in PLAT_CHART_CACHE.get() if (l.matchesName(search,fuzzy) or str(l.id)==search)]
     
     if skills:
@@ -313,47 +307,6 @@ async def _(args: Message = CommandArg()):
     await platsearch.send("\n".join(msg))
     
     
-# platskill = on_command("platskill")
-# @platskill.handle()
-# async def _(args: Message = CommandArg()):
-#     sa=SearchArgs().parse(args.extract_plain_text())
-#     if sa.error:
-#         await platweight.finish(sa.error)
-#         return
-#     search=sa.text
-#     page=sa.page
-    
-#     msg=[]
-#     # If no args present, list all skillsets
-#     if not sa.text:
-#         the_lists=PLAT_SHEET_CACHE.get()
-#         skills=[]
-#         for l in the_lists:
-#             for s in l.skillsets:
-#                 if s not in skills:
-#                     skills.append(s)
-#         msg.append(f"There are {skills.__len__()} skills.")
-#         msg.append(f"Use '-platskill skill1,skill2...' to filter levels by skillsets.")
-#         msg.append(f"Example: -platskill {random.choice(skills)}")
-#         await platskill.send("\n".join(msg))
-#         return
-        
-    
-#     results:list[plat_sheets.TheListsEntry]=skill_in_three_sheets(search)
-    
-#     count=results.__len__()
-#     entries_per_page = 5
-    
-#     results,maxpages,page=select_page(results,count,entries_per_page,page)
-        
-#     if not count:
-#         msg.append(f"No levels found with skill {search}")
-#     else:
-#         msg.append(f"{count} levels found with skills (Page {page}/{maxpages}):")
-#         msg.extend([f"{l.name} by {l.creator}" for l in results])
-    
-#     await platskill.send("\n".join(msg))
-
 def level_in_three_sheets(search:str):
     result:list[plat_sheets.TheListsEntry]=[]
     the_lists=plat_sheets.get_3_lists()
@@ -362,33 +315,6 @@ def level_in_three_sheets(search:str):
             result.append(level)
     return result
 
-# def skill_in_three_sheets(search:str):
-#     result:list[plat_sheets.TheListsEntry]=[]
-#     the_lists=PLAT_SHEET_CACHE.get()
-#     split:list[str]=[t.strip().lower() for t in search.split(",")]
-    
-#     for level in the_lists:
-#         lskills=[s.lower() for s in level.skillsets]
-#         for group in skill_groups:
-#             intersect=list(set(group) & set(lskills))
-#             if intersect:
-#                 lskills=list(set(group) | set(lskills))
-#         matched=True
-#         for s in split:
-#             if s not in lskills:
-#                 matched=False
-#                 break
-#         if matched:
-#             result.append(level)
-#     return result
-
-# L = TypeVar("L", bound=plat_sheets.LevelEntry)
-# def search_in_levels(levels:list[L],search:str,fuzzy:bool=False):
-#     result:list[L]=[]
-#     for level in levels:
-#         if level.matchesName(search,fuzzy):
-#             result.append(level)
-#     return result
 
 plathelp = on_command("plathelp")
 @plathelp.handle()
